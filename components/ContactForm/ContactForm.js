1cfea53e8ce739e601c167922d9c28dd161c2982
unknown <ciastek1996@gmail.com>
2021-03-25 13:17:55 +0100

    connection with netlify forms

--- components/ContactForm/ContactForm.js
+++ components/ContactForm/ContactForm.js
@@ -1,28 +1,77 @@
+import { useState } from "react";
 import styles from "./ContactForm.module.scss";
 import Button from "../Button/Button";
 
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+};
+
 const ContactForm = () => {
+  const [contactData, setContactData] = useState({
+    fullname: "",
+    email: "",
+    content: "",
+  });
+  const [errors, setErrors] = useState([]);
+
+  const handleChange = (e) => {
+    setContactData({ ...contactData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!errors.length) {
+      fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: encode({ "form-name": "contact", ...contactData }),
+      })
+        .then(() => console.log("Form successfully submitted"))
+        .catch((error) => alert(error));
+    }
+  };
+
   return (
     <>
       <form
         name="contact"
         id="contact"
-        action="/success"
+        //action="/success"
         method="POST"
         data-netlify="true"
         className={styles.form}
+        onSubmit={handleSubmit}
       >
         <input type="hidden" name="contact" value="contact"></input>
         <div className={styles.inputWrapper}>
-          <input type="text" name="name" placeholder=" " required />
+          <input
+            type="text"
+            name="name"
+            placeholder=" "
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="name">Full name</label>
         </div>
         <div className={styles.inputWrapper}>
-          <input type="text" name="email" placeholder=" " required />
+          <input
+            type="email"
+            name="email"
+            placeholder=" "
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="name">E-mail</label>
         </div>
         <div className={styles.inputWrapper}>
-          <textarea name="content" placeholder=" " required />
+          <textarea
+            name="content"
+            placeholder=" "
+            onChange={handleChange}
+            required
+          />
           <label htmlFor="name">Write your message...</label>
         </div>
       </form>
